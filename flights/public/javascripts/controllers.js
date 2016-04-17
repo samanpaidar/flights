59a5845e85e97059241ba0181bb308240cdbef8b
samanpaidar <samanpaidar@gmail.com>
2016-04-17 16:12:44 +0200

    komplitera med angular

---
+++ flights/public/javascripts/controllers.js
@@ -0,0 +1,4 @@
+var flightsystem = angular.module('flightsystem', []);
+flightsystem.controller('landvetter', ['$scope', '$http', function($scope, $http) {
+    console.log("Hello World from controller");
+}]);
